122ba2fb16df9e922519801ffb8d5bf60c186e7d
Victor Kaiser-Pendergrast <v.kaiser.pendergrast@gmail.com>
2014-01-21 16:25:13 -0500

    Added a preference type that launches an Activity on launch

---
+++ Library/src/com/victor/kaiser/pendergrast/settings/types/ActivityPreference.java
@@ -0,0 +1,59 @@
+/**
+ * @author Victor Kaiser-Pendergrast
+ */
+
+
+package com.victor.kaiser.pendergrast.settings.types;
+import java.util.List;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.Intent;
+import android.view.View;
+
+import com.victor.kaiser.pendergrast.settings.R;
+import com.victor.kaiser.pendergrast.settings.option.PreferenceOption;
+
+
+public class ActivityPreference extends AbstractPreference {
+
+	private Class mActivityClass;
+	private Context mContext;
+
+	public ActivityPreference(Context context, SharedPreferences prefs, String key, String title, Class activity) {
+		super(prefs, key, title);
+		mContext = context;
+		mActivityClass = activity;
+	}
+
+	public ActivityPreference(Context context, SharedPreferences prefs, String key, String title, int imageResource, Class activity) {
+		super(prefs, key, title, imageResource);
+		mContext = context;
+		mActivityClass = activity;
+	}
+
+	@Override
+	public boolean onSelect(){
+		// Launch the specified Activity
+		Intent launchIntent = new Intent(mContext, mActivityClass);
+		launchIntent.putExtra("preference_key", getKey());
+		mContext.startActivity(launchIntent);
+		return true;
+	}
+
+	@Override
+	public View getCard(Context context){
+		return getDefaultCard(context);
+	}
+
+	@Override
+	public List<PreferenceOption> getOptions() {
+		// onSelect returns true, so this will not be called
+		return null;
+	}
+	
+	@Override
+	public void onOptionSelected(int index){
+		// onSelect returns true, so this will not be called
+	}
+}
